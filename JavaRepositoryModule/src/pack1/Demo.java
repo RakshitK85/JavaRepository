f29a5f3ccfde9303344652ff7a8961d1c3a3fd4b
K299162 <K299162@VW10A26926.principalusa.corp.principal.com>
2022-03-09 11:30:14 +0530

    Java project has been created with simple implementation of MyInterface interface.

---
+++ JavaRepositoryModule/src/pack1/Demo.java
@@ -0,0 +1,12 @@
+package pack1;
+import pack1.MyInterfaceImp1;
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyInterfaceImp1 imp1=new
+				MyInterfaceImp1();
+		imp1.disp();
+	}
+
+}
---
+++ JavaRepositoryModule/src/pack1/MyInterfaceImp1.java
@@ -0,0 +1,10 @@
+package pack1;
+
+public class MyInterfaceImp1 implements 
+MyInterface{
+	public void disp()
+	{
+		System.out.println("inside MyInterfaceImp1 class");
+	}
+
+}
